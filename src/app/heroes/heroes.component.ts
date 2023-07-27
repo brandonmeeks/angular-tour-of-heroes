c7331cc9138f7ed5059472420c6e3530ca0303dc
Brandon <brandon.c.meeks@gmail.com>
2023-07-27 07:40:02 -0500

    adds message component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,7 +14,7 @@ export class HeroesComponent {
   heroes: Hero[] = []
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService){}
+  constructor(private heroService: HeroService, private messageService: MessageService){}
 
   getHeroes(): void{
     this.heroService.getHeroes()
@@ -26,5 +27,6 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
